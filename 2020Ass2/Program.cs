f1aebae2d3d5b6f98e59e0f2143a65f2880afb87
lauramann <lauramann@Dexters-MacBook-Pro.local>
2016-11-08 23:30:34 -0500

    final :)

--- 2020Ass2/Program.cs
+++ 2020Ass2/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 
-
 public enum TStatus { EMPTY, FULL, DELETED }
 
 public class HashTable<TKey, TValue>
 {
-	//	public override string ToString()
-	//{
-
-	//}
+	
 	private class Entry  //stores the key, item, and status of a particular entry of the hash table
 	{
 		public TKey Key { get; set; }
@@ -20,7 +16,10 @@ public class HashTable<TKey, TValue>
 			Item = item;
 			Status = status;
 		}
-
+		public override string ToString()
+		{
+			return string.Format("[Entry: Key={0}, Item={1}, Status={2}]", Key, Item, Status);
+		}
 	}
 
 	private Entry[] table;  //array of entries
@@ -82,87 +81,164 @@ public class HashTable<TKey, TValue>
 
 
 	//Add: adds an item with key to hash table
-	public void Add(TKey key, TValue item, int pos)
+	public void Add(TKey key, TValue item)
 	{
-		if (count + 1 < size)
-			table[++count] = new Entry(key, item, TStatus.FULL);
+		//
+		if (count < size)
+		{
+			if (ContainsKey(key))
+			{
+				for (int i = 0; i < count; i++)
+				{
+					if (table[i].Key.Equals(key))
+					{
+						table[i].Item = item;
+						table[i].Status = TStatus.FULL;
+					}
+				}
+			}
+			else
+			{
+				int index;
+				// linear probing
+				if (scheme == 1)
+				{
+					index = Linear(0);
+				}
+				else
+				{
+					index = Quadratic(0);
+				}
+				table[index].Key = key;
+				table[index].Item = item;
+				table[index].Status = TStatus.FULL;
+			}
+			count++;
+		}
 	}
 
-	//Remove: removes the item with key from the hash table and returns true if done; false otherwise
+	////Remove: removes the item with key from the hash table and returns true if done; false otherwise
 	public bool Remove(TKey key)
 	{
-		if (table[key].Item = null)
-			return false;
-		else if (table[key].Item != null)
+		if (count > 0)
 		{
-			table.RemoveAt(key);
-			table[key].Status = TStatus.DELETED;
-			return true;
+			for (int i = 0; i < size; i++)
+			{
+				if (table[i].Key.Equals(key))
+				{
+					table[i].Status = TStatus.DELETED;
+					count--;
+					return true;
+				}
+			}
 		}
+		return false;
 	}
 
 	//ContainsKey: returns true if the key is used in the hash table; false otherwise
 	public bool ContainsKey(TKey key)
 	{
-		if (table[key].Key = null)
-			return false;
-		else if (table[key].Key != null)
-			return true;
+		if (count > 0)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				if (table[i].Key.Equals(key))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	//Retrieve: returns the item with key if found; default(TValue) otherwise
 	public TValue Retrieve(TKey key)
 	{
-		int retrivedKey = table.GetValue(table[key].Item);
-		return retrivedKey;
+		if (count > 0)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				if (table[i].Key.Equals(key))
+				{
+					return table[i].Item;
+				}
+			}
+		}
+		return default(TValue);
 	}
+}
 
-	public int Linear(int i)  //uses linear probing to return the next availible (EMPTY/DELETED) index in the table
-	{
-		//for (int j = i; i < size; i++)
-		return 0;
-	}
+//uses linear probing to return the next availible (EMPTY/DELETED) index in the table
+private int Linear(int i)
+{
+	if (count >= 0.72 * size)
+		doubleTableSize();
 
-	private int Quadratic(int i)  //uses quadratic probing to return the next available (EMPTY/DELETED) index in the table
+	for (int index = i; index < size; index++)
 	{
+		if (table[index].Status == TStatus.EMPTY || table[index].Status == TStatus.DELETED)
+			return index;
+	}
+	return -1;
+}
 
-	}
-	public int Size()
+//uses quadratic probing to return the next available (EMPTY/DELETED) index in the table
+private int Quadratic(int i)
+{
+	if (count >= 0.50 * size)
+		doubleQuadraticTableSize();
+
+	for (int index = i; index < size; index++)
 	{
-		return size;
+		if (table[index].Status == TStatus.EMPTY || table[index].Status == TStatus.DELETED)
+			return index;
 	}
+	return -1;
+}
+
+private static Coordinate getRandomCoordinate(Random rand)
+{
+	int lati = 0, longi = 0;
+
+	lati = rand.Next(100);
+	longi = rand.Next(100);
+	return new Coordinate(longi, lati);
 }
 
 public class Coordinate  //used as key
 {
-	private int longitude = 18;  //from 0...99
-	private int latitude = 19;  //from 0...99
+	private int longitude;  //from 0...99
+	private int latitude;  //from 0...99
 	private int size;
 
+    //
 	public Coordinate(int longitude, int latitude, int size)
 	{
 		this.longitude = longitude;
 		this.latitude = latitude;
 		this.size = size;
+
 	}
 
-	public override bool Equals(object obj)  //from the Object class
+	public override bool Equals(object obj)
 	{
-		Coordinate other = obj as Coordinate;
-		if (obj == null)
-			return false;
-		else
-			return longitude.Equals(other.longitude) && latitude.Equals(other.latitude);
+		var coord = obj as Coordinate;
+		return latitude == coord.latitude && longitude == coord.longitude;
 	}
 
-	public override int GetHashCode()  //from the Object class
+	public override int GetHashCode()
 	{
 		int itemSqrd = longitude * latitude;
 		//find square of item
 		int hashKey = itemSqrd % size;
-		return hashKey;
+		return latitude.GetHashCode() ^ longitude.GetHashCode();
 	}
+}
 
+	public override string ToString()
+	{
+		return latitude + "," + longitude;
+	}
 }
 
 public class City
@@ -176,13 +252,68 @@ public class City
 		this.population = population;
 	}
 }
-public static class Test
+public static class Program
 {
-	public static void Main()
+	//Main method
+	static void Main(string[] args)
 	{
-		//creates new instance of the HashTable class with paramters (size & scheme)
-		HashTable<Coordinate, City> test = new HashTable<Coordinate, City>(7, 1);
-		Coordinate coordinate = new Coordinate(5, 2, test.Size());
-		test.Add(coordinate, new City("Boston", 90), coordinate.GetHashCode());
+		int linearCollisions = 0;
+		int sumLinearCollisions = 0;
+		int quadraticCollisions = 0;
+		int sumQuadraticCollisions = 0;
+		int avgLinearCollisions;
+		int avgQuadraticCollisions;
+
+		// linear probing table
+		HashTable<Coordinate, City> linearHashTable = new HashTable<Coordinate, City>(13, 1);
+
+		// quadratic probing table
+		HashTable<Coordinate, City> quadraticHashTable = new HashTable<Coordinate, City>(13, 2);
+
+		Coordinate coordinate;
+
+		// insert 1000 cities
+		for (int x = 0; x < 20; x++)
+		{
+			linearCollisions = 0;
+			quadraticCollisions = 0;
+
+			for (int i = 0; i < 1000; i++)
+			{
+				Random rand = new Random();
+				do
+				{
+					coordinate = getRandomCoordinate(rand);
+					linearCollisions++;
+				} while (linearHashTable.ContainsKey(coordinate));
+
+				linearHashTable.Add(coordinate, new City("Peterborough", 10845));
+			}
+
+			for (int i = 0; i < 1000; i++)
+			{
+				Random rand = new Random();
+				do
+				{
+					coordinate = getRandomCoordinate(rand);
+					quadraticCollisions++;
+				} while (quadraticHashTable.ContainsKey(coordinate));
+
+				quadraticHashTable.Add(coordinate, new City("Peterborough", 10845));
+				Console.WriteLine(quadraticHashTable);
+			}
+
+			sumLinearCollisions = sumLinearCollisions + linearCollisions;
+			sumQuadraticCollisions = sumQuadraticCollisions + quadraticCollisions;
+		}
+
+		// calculate average collisions
+		avgLinearCollisions = sumLinearCollisions / 20;
+		avgQuadraticCollisions = sumQuadraticCollisions / 20;
+
+		// display average number of collisions
+		Console.WriteLine("Number of average collsions in Linear Probing: " + avgLinearCollisions);
+		Console.WriteLine("Number of average collsions in Quadratic Probing: " + avgQuadraticCollisions);
 	}
 }
+		    
